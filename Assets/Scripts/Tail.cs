0ff367d75141f61876b8d637f6e6f1fa4ee1d01b
Mustafa İpek <33962976+mistova@users.noreply.github.com>
2021-04-29 05:26:17 +0300

    Hook Added

--- Assets/Scripts/Tail.cs
+++ Assets/Scripts/Tail.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Tail : MonoBehaviour
@@ -5,6 +6,12 @@ public class Tail : MonoBehaviour
 
     [SerializeField]
     GameObject[] tails;
+
+    [SerializeField]
+    Camera camera;
+
+    [SerializeField]
+    float speed;
 
     int count;
 
@@ -19,11 +26,28 @@ public class Tail : MonoBehaviour
             ExtendTail(1);
         else if (Input.GetKeyDown(KeyCode.Backspace))
             ShortenTail(1);
+        else if (Input.GetMouseButton(0))
+            Hook();
+    }
+
+    private void Hook()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Vector3 vec = hit.transform.position - tails[i].transform.position;
+                tails[i].GetComponent<Rigidbody>().AddForce(vec.normalized * speed);
+            }
+        }
     }
 
     public void ExtendTail(int ex)
     {
-        if (count < tails.Length)
+        for(int i = 0; i < ex && count < tails.Length; i++)
         {
             MeshRenderer[] sp = tails[count].GetComponentsInChildren<MeshRenderer>();
             tails[count].GetComponent<Collider>().enabled = true;
@@ -34,13 +58,14 @@ public class Tail : MonoBehaviour
                 sp[0].GetComponent<MeshRenderer>().enabled = true;
                 sp[1].GetComponent<MeshRenderer>().enabled = false;
             }
-            count += ex;
+            count ++;
         }
     }
     public void ShortenTail(int sh)
     {
-        if(count > 0) { 
-            count -= sh;
+        for (int i = 0; i < sh && count > 0; i++)
+        { 
+            count --;
             MeshRenderer[] sp = tails[count].GetComponentsInChildren<MeshRenderer>();
             tails[count].GetComponent<Collider>().enabled = false;
             sp[1].GetComponent<MeshRenderer>().enabled = false;
